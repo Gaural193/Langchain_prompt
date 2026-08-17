1bb46d064eaa782f632939eb1792b3571791068d
Gaural Makwana <makgaural193@gmail.com>
2026-08-17 16:13:19 -0400

    A: improt template from json file

--- prompt_ui.py
+++ prompt_ui.py
@@ -1,7 +1,8 @@
 from langchain_openai import ChatOpenAI
 from dotenv import load_dotenv
 import streamlit as st
-from langchain_core.prompts import PromptTemplate
+from langchain_core.prompts import PromptTemplate , load_prompt
+#load_prompt is for importing template from the prompt_generator.py
 
 
 load_dotenv()
@@ -17,24 +18,37 @@ style_input = st.selectbox("Select Explanation Style: ", ["Beginner-friendly", "
 
 length_input = st.selectbox("Serlet Explanation Length", ["Short(1-2 paragraph)", "Medium(3-5 paragraph)", "Long(detailed explanation)"])
 
-template = PromptTemplate(
-    template=""" 
-    Please summarize the research paper titled "{paper_input}" with the following specifications:
-Explanation Style: {style_input}
-Explanation Length: {length_input}
-1. Mathematical Details:
-    - Include relevant mathematical equations if present in the paper.
-    - Explain the mathematical concepts using simple, intuitive code snippets where
-      applicable.
-2. Analogies:
-    - Use relatable analogies to simplify complex ideas.
-If certain information is not available in the paper, respond with: "Insufficient
-information available" instead of guessing.
-Ensure the summary is clear, accurate, and aligned with the provided style and length.
- """
-)
+
+# -----------------------------------------------------------------
+# Now the below template is comming from the prompt_generator.py file 
+# template = PromptTemplate(
+#     template=""" 
+#     Please summarize the research paper titled "{paper_input}" with the following specifications:
+# Explanation Style: {style_input}
+# Explanation Length: {length_input}
+# 1. Mathematical Details:
+#     - Include relevant mathematical equations if present in the paper.
+#     - Explain the mathematical concepts using simple, intuitive code snippets where
+#       applicable.
+# 2. Analogies:
+#     - Use relatable analogies to simplify complex ideas.
+# If certain information is not available in the paper, respond with: "Insufficient
+# information available" instead of guessing.
+# Ensure the summary is clear, accurate, and aligned with the provided style and length.
+#  """,
+# input_variables = ['paper_input','style_input','length_input'],
+# validate_template=True #this will give us a error message wjen we there is extra or less parameter in above input_variable
+# )
+
+template = load_prompt('template.json')
+prompt = template.invoke({
+    'paper_input':paper_input,
+    'style_input': style_input,
+    'length_input':length_input
+})
 
 if st.button("Summarize"):
-    # result= model.invoke(user_input)
-    st.write("Hello")
+    result = model.invoke(prompt)
+    st.write(result.content)
 
+# Now if we want to 
